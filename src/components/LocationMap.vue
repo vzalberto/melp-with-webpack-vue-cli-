f19edc1e8053d4802ea0ea855f02f13c2909dd5d
Alberto <a>
2017-11-01 19:27:35 -0600

    Add LocationMap Component

--- src/components/LocationMap.vue
+++ src/components/LocationMap.vue
@@ -1,10 +1,18 @@
 <template>
 <div class="col-sm-12 col-lg-6 map-container hidden-sm-down">
-        <gmap-map
+    <gmap-map
         :center="center"
         :zoom="zoom"
         style="width: 100%; height: 100%;"
         >
+
+            <gmap-marker
+              :key="index"
+              v-for="(m, index) in locations"
+              :position="m.address.location"
+              :clickable="true"
+              @click="center=m.position"
+            ></gmap-marker>
 
     </gmap-map>
 </div>
